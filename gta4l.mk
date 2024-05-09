5e35b0022618c2a6c2c252e4ed873188af10ee89
Tim Zimmermann <tim@linux4.de>
2024-05-09 06:48:24 +0200

    gta4l-common: Use pixel power HAL directly
    * Samsung one is outdated and qti ones are incompatible
    
    Change-Id: I65378563396d12ee76e7f2b34105988c7e66cf38

--- gta4l.mk
+++ gta4l.mk
@@ -290,7 +290,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power@1.2.vendor \
     libqti-perfd-client
 
