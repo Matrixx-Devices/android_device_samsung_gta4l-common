f285e5ed44938397795ccc12b95952a0f75bbbba
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    gta4l-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- gta4l.mk
+++ gta4l.mk
@@ -147,7 +147,6 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.bengal \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
